1c5a3e21f3f14e7e7856294b7fd82445f4ec0e92
van <noworktoday00@gmail.com>
2022-09-14 17:53:59 +0800

    Update 2022-09-14T09:53:56.419Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -4,121 +4,121 @@ $(document).ready(function (e) {
   $('img[usemap]').rwdImageMaps();
 });
 $(".new-taipei").mouseenter(function () {
-  $(".icon-newtaipei").css("max-width", "30px");
+  $(".icon-newtaipei").css("max-width", "32px");
 });
 $(".new-taipei").mouseleave(function () {
   $(".icon-newtaipei").css("max-width", "24px");
 });
 $(".kee-lung").mouseenter(function () {
-  $(".icon-keelung").css("max-width", "30px");
+  $(".icon-keelung").css("max-width", "32px");
 });
 $(".kee-lung").mouseleave(function () {
   $(".icon-keelung").css("max-width", "24px");
 });
 $(".taipei").mouseenter(function () {
-  $(".icon-taipei").css("max-width", "30px");
+  $(".icon-taipei").css("max-width", "32px");
 });
 $(".taipei").mouseleave(function () {
   $(".icon-taipei").css("max-width", "24px");
 });
 $(".kin-men").mouseenter(function () {
-  $(".icon-kinmen").css("max-width", "30px");
+  $(".icon-kinmen").css("max-width", "32px");
 });
 $(".kin-men").mouseleave(function () {
   $(".icon-kinmen").css("max-width", "24px");
 });
 $(".ma-tsu").mouseenter(function () {
-  $(".icon-matsu").css("max-width", "30px");
+  $(".icon-matsu").css("max-width", "32px");
 });
 $(".ma-tsu").mouseleave(function () {
   $(".icon-matsu").css("max-width", "24px");
 });
 $(".peng-hu").mouseenter(function () {
-  $(".icon-penghu").css("max-width", "30px");
+  $(".icon-penghu").css("max-width", "32px");
 });
 $(".peng-hu").mouseleave(function () {
   $(".icon-penghu").css("max-width", "24px");
 });
 $(".tao-yuan").mouseenter(function () {
-  $(".icon-taoyuan").css("max-width", "30px");
+  $(".icon-taoyuan").css("max-width", "32px");
 });
 $(".tao-yuan").mouseleave(function () {
   $(".icon-taoyuan").css("max-width", "24px");
 });
 $(".hsin-chu").mouseenter(function () {
-  $(".icon-hsinchu").css("max-width", "30px");
+  $(".icon-hsinchu").css("max-width", "32px");
 });
 $(".hsin-chu").mouseleave(function () {
   $(".icon-hsinchu").css("max-width", "24px");
 });
 $(".miao-li").mouseenter(function () {
-  $(".icon-miaoli").css("max-width", "30px");
+  $(".icon-miaoli").css("max-width", "32px");
 });
 $(".miao-li").mouseleave(function () {
   $(".icon-miaoli").css("max-width", "24px");
 });
 $(".tai-chung").mouseenter(function () {
-  $(".icon-taichung").css("max-width", "30px");
+  $(".icon-taichung").css("max-width", "32px");
 });
 $(".tai-chung").mouseleave(function () {
   $(".icon-taichung").css("max-width", "24px");
 });
 $(".chang-hua").mouseenter(function () {
-  $(".icon-changhua").css("max-width", "30px");
+  $(".icon-changhua").css("max-width", "32px");
 });
 $(".chang-hua").mouseleave(function () {
   $(".icon-changhua").css("max-width", "24px");
 });
 $(".yun-lin").mouseenter(function () {
-  $(".icon-yunlin").css("max-width", "30px");
+  $(".icon-yunlin").css("max-width", "32px");
 });
 $(".yun-lin").mouseleave(function () {
   $(".icon-yunlin").css("max-width", "24px");
 });
 $(".chia-yi").mouseenter(function () {
-  $(".icon-chiayi").css("max-width", "30px");
+  $(".icon-chiayi").css("max-width", "32px");
 });
 $(".chia-yi").mouseleave(function () {
   $(".icon-chiayi").css("max-width", "24px");
 });
 $(".tai-nan").mouseenter(function () {
-  $(".icon-tainan").css("max-width", "30px");
+  $(".icon-tainan").css("max-width", "32px");
 });
 $(".tai-nan").mouseleave(function () {
   $(".icon-tainan").css("max-width", "24px");
 });
 $(".kao-hsiung").mouseenter(function () {
-  $(".icon-kaohsiung").css("max-width", "30px");
+  $(".icon-kaohsiung").css("max-width", "32px");
 });
 $(".kao-hsiung").mouseleave(function () {
   $(".icon-kaohsiung").css("max-width", "24px");
 });
 $(".ping-tung").mouseenter(function () {
-  $(".icon-pingtong").css("max-width", "30px");
+  $(".icon-pingtong").css("max-width", "32px");
 });
 $(".ping-tung").mouseleave(function () {
   $(".icon-pingtong").css("max-width", "24px");
 });
 $(".tai-tung").mouseenter(function () {
-  $(".icon-taitung").css("max-width", "30px");
+  $(".icon-taitung").css("max-width", "32px");
 });
 $(".tai-tung").mouseleave(function () {
   $(".icon-taitung").css("max-width", "24px");
 });
 $(".hua-lian").mouseenter(function () {
-  $(".icon-hualien").css("max-width", "30px");
+  $(".icon-hualien").css("max-width", "32px");
 });
 $(".hua-lian").mouseleave(function () {
   $(".icon-hualien").css("max-width", "24px");
 });
 $(".yi-lan").mouseenter(function () {
-  $(".icon-yilan").css("max-width", "30px");
+  $(".icon-yilan").css("max-width", "32px");
 });
 $(".yi-lan").mouseleave(function () {
   $(".icon-yilan").css("max-width", "24px");
 });
 $(".nan-tou").mouseenter(function () {
-  $(".icon-nantou").css("max-width", "30px");
+  $(".icon-nantou").css("max-width", "32px");
 });
 $(".nan-tou").mouseleave(function () {
   $(".icon-nantou").css("max-width", "24px");
